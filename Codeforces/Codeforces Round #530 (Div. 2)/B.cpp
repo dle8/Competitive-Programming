fa806be6c39eca4d92a7bc57503cbd2e252e94ce
dle8 <dle8@u.rochester.edu>
2019-01-19 20:21:15 -0500

    Just implementation

---
+++ Codeforces/Codeforces Round #530 (Div. 2)/B.cpp
@@ -0,0 +1,105 @@
+/*
+Author: Dung Tuan Le
+University of Rochester
+*/
+
+#include <bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define FOR(i, l, r) for (int i=l; i<=r; i++)
+#define REP(i, r, l) for (int i=r; i>=l; i--)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) int(x.size())
+#define eps 1e-9
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pii;
+typedef pair<long, long> pll;
+typedef pair<ll, ll> pllll;
+typedef pair<ld, ld> Point;
+typedef pair<Point, Point> line;
+struct strLine { ld a, b, c; };
+
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<ll> vll;
+
+const double pi = 3.141592653589793;
+
+ll gcd(ll a, ll b, ll &x, ll &y) {
+    if (a == 0) {
+        x = 0; y = 1;
+        return b;
+    }
+    ll x1, y1;
+    ll d = gcd(b%a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return d;
+}
+
+ll lcm(ll a, ll b) { return (a*b)/__gcd(a, b); }
+ll max(ll a, ll b) { return (a>=b)?a:b; }
+ll min(ll a, ll b) { return (a<=b)?a:b; }
+
+/* tach xau
+string str = "Hello World"; stringstream ss(str);
+while (ss >> token) cout << token << endl;
+*/
+
+int main() {
+	fio;
+  string s;
+  ll k;
+  cin >> s;
+  cin >> k;
+  ll candy = 0, flake = 0, norm = 0;
+  FOR(i, 0, sz(s) - 1) {
+    candy += (s[i] == '?');
+    flake += (s[i] == '*');
+    norm += (s[i] != '*' && s[i] != '?');
+  }
+  if (norm == k) {
+    FOR(i, 0, sz(s) - 1) if (s[i] != '*' && s[i] != '?') cout << s[i];
+  }
+  else if (norm > k) {
+    ll cnt = norm;
+    vector<char> v;
+    FOR(i, 0, sz(s) - 1) {
+      if (s[i] != '*' && s[i] != '?') v.pb(s[i]), cnt--;
+      else if (sz(v) != 0) v.pop_back();
+      if (sz(v) + cnt == k) {
+        FOR(j, i + 1, sz(s) - 1) if (s[j] != '*' && s[j] != '?' ) v.pb(s[j]);
+        break;
+      }
+    }
+    if (sz(v) != k) return cout << "Impossible", 0;
+    FOR(i, 0, sz(v) - 1) cout << v[i];
+  }
+  else {
+    if (flake == 0) return cout << "Impossible", 0;
+    ll cnt = norm;
+
+    vector<char> v;
+    FOR(i, 0, sz(s) - 1) {
+      if (s[i] == '?') v.pop_back();
+      else if (s[i] == '*') {
+        long tmp = sz(v);
+        FOR(j, 1, k - tmp - cnt) v.pb(v.back());
+        FOR(j, i + 1, sz(s) - 1) if (s[j] != '*' && s[j] != '?') v.pb(s[j]);
+        FOR(j, 0, sz(v) - 1) cout << v[j];
+        break;
+      }
+      else v.pb(s[i]), cnt--;
+    }
+  }
+	return 0;
+}
